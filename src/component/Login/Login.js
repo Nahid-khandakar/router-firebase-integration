7eeb207318cfa5fa9d27836daedec5d46865f30b
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-10 01:08:53 +0600

    coustomize log in and register  form

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -3,8 +3,18 @@ import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h2>this is login div</h2>
+        <div className='loginForm'>
+            <h2>Log In Here</h2>
+            <hr />
+            <form >
+                <input type="email" className='email-field' placeholder='email' />
+                <br />
+                <input type="password" className='password-field ' placeholder='password' />
+                <br />
+                <input type="submit" value="Log In" className='submit-btn' />
+
+                <input type="submit" value="Use Google" className='submit-btn-google' />
+            </form>
         </div>
     );
 };
--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -3,8 +3,18 @@ import './Register.css'
 
 const Register = () => {
     return (
-        <div>
-            <h1>This is register</h1>
+        <div className='loginForm'>
+            <h2>Please Register Here</h2>
+            <hr />
+            <form >
+                <input type="text" className='name-field' placeholder='Your name' />
+                <br />
+                <input type="email" className='email-field' placeholder='Your email' />
+                <br />
+                <input type="password" className='password-field ' placeholder='password' />
+                <br />
+                <input type="submit" value="Register" className='submit-btn' />
+            </form>
         </div>
     );
 };
